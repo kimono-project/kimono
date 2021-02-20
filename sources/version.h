5dd5f96181b479eff0d476bed199f2a8e99bcf0d
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-02-20 11:05:28 +0100

    fixed indention

--- sources/version.h
+++ sources/version.h
@@ -5,9 +5,9 @@
 #include <string>
 
 static const int CLIENT_VERSION = 1000000 * CLIENT_VERSION_MAJOR
-                                +   10000 * CLIENT_VERSION_MINOR
-                                +     100 * CLIENT_VERSION_REVISION
-                                +       1 * CLIENT_VERSION_BUILD;
++   10000 * CLIENT_VERSION_MINOR
++     100 * CLIENT_VERSION_REVISION
++       1 * CLIENT_VERSION_BUILD;
 
 extern const std::string CLIENT_NAME;
 extern const std::string CLIENT_BUILD;
